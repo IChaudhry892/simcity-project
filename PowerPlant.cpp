fd165b24e17468cf5d1982edcf80a45e8e724aa2
Ibrahim Chaudhry <IbrahimChaudhry@my.unt.edu>
2024-10-26 12:50:40 -0500

    Removed old commented out code from Powerline class

--- PowerPlant.cpp
+++ PowerPlant.cpp
@@ -2,15 +2,7 @@
 #include <iostream>
 using namespace std;
 
-// PowerPlant::PowerPlant(){
-//     type = 'P';
-// }
-
 PowerPlant::PowerPlant() : MapObject('P') {}
-
-// PowerPlant::PowerPlant(char t){
-//     type = t;
-// }
 
 PowerPlant::PowerPlant(char t) : MapObject(t) {}
 
