dab634634608ca72ba10dcb3c2167fae39e8fd07
Ibrahim Chaudhry <IbrahimChaudhry@my.unt.edu>
2024-10-26 12:47:05 -0500

    Removed the virtual growFunction from the Zone parent class and all its derived zone classes b/c its functionality was replaced by the virtual evaluateGrowth function

--- ResidentialZone.h
+++ ResidentialZone.h
@@ -9,6 +9,5 @@ public:
     int getPopulation() const override;
     void setPopulation(int pop) override;
 
-    void growFunction(std::vector<std::vector<MapObject*>>& region, int x, int y, SimCity& city) override;
     int evaluateGrowth(std::vector<std::vector<MapObject*>>& region, int x, int y, SimCity& city) override;
 };
