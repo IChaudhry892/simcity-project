74fdf233f39e6891f428cc56500b1e171e45cd85
Ibrahim Chaudhry <IbrahimChaudhry@my.unt.edu>
2024-10-26 12:49:38 -0500

    Removed old commented out code from MapObject class

--- MapObject.cpp
+++ MapObject.cpp
@@ -2,19 +2,10 @@
 #include <iostream>
 using namespace std;
 
-// MapObject::MapObject(){
-//     type = 'n';
-// }
 MapObject::MapObject() : type('n'), pollution(0) {}
 
-// MapObject::MapObject(char t){
-//     type = t;
-// }
 MapObject::MapObject(char t) : type(t), pollution(0) {}
 
-// char MapObject::getType() const{
-//     return 0;
-// }
 int MapObject::getPollution() const{
     return pollution;
 }
